8b413354a01786f9557901d5aea747d10b4e6b49
wzthor <wzthor2002@gmail.com>
2022-11-08 13:34:04 +0800

    #W09-01
    - Added notification for successful registration
    - Reduced redundancy in code for showing notification

--- Register.aspx.designer.cs
+++ Register.aspx.designer.cs
@@ -60,6 +60,15 @@ namespace RojakJelah
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl notification;
 
         /// <summary>
+        /// notificationIcon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl notificationIcon;
+
+        /// <summary>
         /// notificationTitle control.
         /// </summary>
         /// <remarks>
